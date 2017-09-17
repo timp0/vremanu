60637e6abe3b885a501088a15c0c1423d165ae28
Ubuntu <ubuntu@ip-172-31-86-212.ec2.internal>
2017-09-17 15:57:35 +0000

    kraken install script

--- kraken_install.sh
+++ kraken_install.sh
@@ -3,6 +3,21 @@
 ##using an ubuntu 16.04 lts r4.4xlarge instance, with full upgrade as of 090517
 ##also emacs, htop, build-essential, ess
 
+if [ "$1" == "centrifuge.make" ]; then
+    cd /home/ubuntu
+    git clone https://github.com/infphilo/centrifuge
+    make
+    sudo make install prefix=/usr/local    
+
+fi
+
+if [ "$1" == "centrifuge.stdidx" ]; then
+    cd /home/ubuntu/centrifuge/indices
+    make p+h+v
+    
+fi
+
+
 if [ "$1" == "kraken.make" ]; then
 
     mkdir -p /home/ubuntu/src
@@ -11,6 +26,8 @@ if [ "$1" == "kraken.make" ]; then
     cd kraken
     mkdir -p /home/ubuntu/kraken/
     ./install_kraken.sh /home/ubuntu/kraken/
+
+    conda install -c bioconda blast
 fi
    
 if [ "$1" == "jellyfish.make" ]; then
@@ -39,8 +56,7 @@ if [ "$1" == "conda" ]; then
 fi
 
 
-
-if [ "$1" == "kraken.db" ]; then
+if [ "$1" == "kraken.db.prep" ]; then
     export LD_LIBRARY_PATH="/usr/local/lib"
     cd /home/ubuntu/
     mkdir -p /home/ubuntu/krakendb
@@ -50,21 +66,57 @@ if [ "$1" == "kraken.db" ]; then
     ~/kraken/kraken-build --download-library viral --db krakendb
     ~/kraken/kraken-build --download-library archaea --db krakendb
     ~/kraken/kraken-build --download-library human --db krakendb
-    ~/kraken/kraken-build --threads 14 --build --jellyfish-hash-size 12800M --db krakendb
-    ##~/kraken/kraken-build --threads 14 --build --db krakendb 
+
+    #filter with Dustmasker and convert low complexity regions to N's with Sed (skipping headers)
+    for i in `find krakendb \( -name '*.fna' -o -name '*.ffn' \)`
+    do
+	echo $i
+	dustmasker -in $i -infmt fasta -outfmt fasta | sed -e '/>/!s/a\|c\|g\|t/N/g' > tempfile
+	mv tempfile $i
+    done
+    
+    ##~/kraken/kraken-build --threads 30 --build --jellyfish-hash-size 25600M --db krakendb
     
 fi
 
-if [ "$1" == "clean.db" ]; then
+if [ "$1" == "kraken.db.k31" ]; then
+    export LD_LIBRARY_PATH="/usr/local/lib"
+    cd /home/ubuntu/
 
+    ~/kraken/kraken-build --threads 30 --build --jellyfish-hash-size 25600M --db krakendb
+
+fi
+
+if [ "$1" == "kraken.db.k24" ]; then
+    export LD_LIBRARY_PATH="/usr/local/lib"
+    cd /home/ubuntu/
+
+    ~/kraken/kraken-build --threads 30 --build --kmer-len 24 --jellyfish-hash-size 25600M --db krakendb
+
+fi
+
+if [ "$1" == "kraken.db.tx31" ]; then
+    aws s3 sync s3://timpawsanalysis/170913_krakendb.masked.fullstd/ krakendb/
+
+fi
+
+if [ "$1" == "kraken.db.tx24" ]; then
+    aws s3 sync s3://timpawsanalysis/170913_krakendb.masked.fullk24/ krakendb/
+
+fi
+
+
+
+if [ "$1" == "clean.db" ]; then
+    echo "Cleaning db"
     cd /home/ubuntu
-    rm -Rf kraken.db
+    rm -Rf krakendb
 
 fi
 
 
 if [ "$1" == "clean.all" ]; then
-
+    echo "Cleaning all"
     cd /home/ubuntu
     rm -Rf kraken
     rm -Rf src
